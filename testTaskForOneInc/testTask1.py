49a3deeeb526c1c8c8830ade1ecbd07dd9c3b40b
Andrey-Shatunov <andreshatuno@gmail.com>
2019-11-08 11:22:23 +0500

    Update testTask1.py

--- testTaskForOneInc/testTask1.py
+++ testTaskForOneInc/testTask1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -5,6 +6,8 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+
+MODEL = 'Lenovo'
 
 
 class TestSearchYandexMarket(unittest.TestCase):
@@ -15,24 +18,33 @@ class TestSearchYandexMarket(unittest.TestCase):
         # устанавливаем размер окна во весь экран
         self.driver.maximize_window()
         self.wait = WebDriverWait(self.driver, 5)
- 
+
         self.driver.get("https://market.yandex.ru/")
         self.driver.find_element(By.LINK_TEXT, 'Компьютерная техника').click()
         self.driver.find_element(By.LINK_TEXT, 'Ноутбуки').click()
-        self.driver.find_element(By.CSS_SELECTOR, '.\\_1-l0XiE_ze:nth-child(8) .\\_2RDCAZB4Gk .LhMupC0dLR').click()
+        self.driver.find_element(
+            By.XPATH,
+            '//a/label/div[contains(.//span, "{0}")]'.format(MODEL)).click()
+
         self.driver.find_element(By.ID, "glpricefrom").send_keys("25000")
         self.driver.find_element(By.ID, "glpriceto").send_keys("30000")
         self.driver.find_element(By.ID, "glpriceto").send_keys(Keys.ENTER)
-     
+
         # при установке фильтра появляется div.preloadable__preloader.preloadable__preloader_visibility_visible.preloadable__paranja
         # который делает область товаров неактивной. Ждем пока он появиться и пропадет
-        self.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > div.main > div:nth-child(7) > div.layout.layout_type_search.i-bem > div.layout__col.i-bem.layout__col_search-results_normal > div.n-filter-applied-results.metrika.b-zone.i-bem.n-filter-applied-results_js_inited.b-zone_js_inited > div > div.preloadable__preloader.preloadable__preloader_visibility_visible.preloadable__paranja')))
-        self.wait.until_not(EC.presence_of_element_located((By.CSS_SELECTOR, 'body > div.main > div:nth-child(7) > div.layout.layout_type_search.i-bem > div.layout__col.i-bem.layout__col_search-results_normal > div.n-filter-applied-results.metrika.b-zone.i-bem.n-filter-applied-results_js_inited.b-zone_js_inited > div > div.preloadable__preloader.preloadable__preloader_visibility_visible.preloadable__paranja')))
-
+        self.wait.until(
+            EC.presence_of_element_located((
+                By.CSS_SELECTOR,
+                'body > div.main > div:nth-child(7) > div.layout.layout_type_search.i-bem > div.layout__col.i-bem.layout__col_search-results_normal > div.n-filter-applied-results.metrika.b-zone.i-bem.n-filter-applied-results_js_inited.b-zone_js_inited > div > div.preloadable__preloader.preloadable__preloader_visibility_visible.preloadable__paranja'
+            )))
+        self.wait.until_not(
+            EC.presence_of_element_located((
+                By.CSS_SELECTOR,
+                'body > div.main > div:nth-child(7) > div.layout.layout_type_search.i-bem > div.layout__col.i-bem.layout__col_search-results_normal > div.n-filter-applied-results.metrika.b-zone.i-bem.n-filter-applied-results_js_inited.b-zone_js_inited > div > div.preloadable__preloader.preloadable__preloader_visibility_visible.preloadable__paranja'
+            )))
 
     def tearDown(self):
         self.driver.close()
-
 
     def test_search_by_price(self):
         # сохраняем html-код страницы в переменую и парсим с помощью BeautifulSoup
@@ -40,10 +52,12 @@ class TestSearchYandexMarket(unittest.TestCase):
         soup = BeautifulSoup(html_source, 'html.parser')
 
         # получаем цены
-        soup_prices_on_page = soup.find_all('div',  {'class':'price'})
+        soup_prices_on_page = soup.find_all('div', {'class': 'price'})
 
         for i in soup_prices_on_page:
-            price = int(i.text.replace('от','').replace('₽','').replace(' ','').strip())
+            price = int(
+                i.text.replace('от', '').replace('₽', '').replace(' ',
+                                                                  '').strip())
             isApple = True if price >= 25000 and price <= 30000 else False
             self.assertTrue(isApple)
 
